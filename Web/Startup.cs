e9f28ef93a3accb31b2efca540b0fe66b177b7a3
dj <djwayne3@gmail.com>
2018-01-10 14:40:36 -0600

    Created a blank web proj

---
+++ Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonalityAppWeb.Startup))]
+namespace PersonalityAppWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
